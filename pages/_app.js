f0865a5b3ebf49930b7b9afd5fe37b960529fa43
miyacoubi-wq <miyacoubi@gmail.com>
2025-10-23 22:17:45 +0100

    fix: corrected CSS import path

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
 import '../styles/globals.css';
-export default function App ({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
